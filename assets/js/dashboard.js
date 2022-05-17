ffafbf1a988e503d37302c0f4a8529b6b64406e3
Nafiu Mubashir <mubashirnafiu@gmail.com>
2022-05-17 06:31:20 +0100

    I added the total debit and total credit feature to the dashboard

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -17,15 +17,18 @@ if (checkUser == null) {
   let myLocalStorage = JSON.parse(localStorage.getItem("currentUsers"));
   acctName.innerText = `Welcome back ${myLocalStorage.firstname} ${myLocalStorage.lastname}`;
   // acctNames.innerText = `${myLocalStorage.firstname} ${myLocalStorage.lastname}`;
-  acctBal.innerHTML = `$ ${myLocalStorage.accountBalance} <sub>USD</sub>`;
+  acctBal.innerHTML = `${myLocalStorage.accountBalance} <sub>USD</sub>`;
   acctNo.innerHTML = `Acct No:  ${myLocalStorage.acctNum}`;
   // console.log(myLocalStorage.histories);
   let transferLog = myLocalStorage.histories.transferHistory;
   let depositLog = myLocalStorage.histories.depositHistory;
   let withdrawLog = myLocalStorage.histories.withdrawHistory;
+  let totalDebit = 0;
+  let totalCredit = 0;
   for (let i = 0; i < transferLog.length; i++) {
     if (transferLog[i].sender) {
       one.innerHTML = `<tr class="text-center"> <td>${transferLog[i].sender}</td> <td class="text-danger">$${transferLog[i].amountSent}</td> <td>${transferLog[i].time}</td> <td>${transferLog[i].date}</td></tr>`
+      // credit.innerHTML = "$ " + transferLog[i].amountSent
     }
     if (transferLog[i].reciver) {
       one.innerHTML = `<tr class="text-center"> <td>${transferLog[i].reciever}</td> <td class="text-success">$${transferLog[i].amountSent}</td>  <td>${transferLog[i].time}</td> <td>${transferLog[i].date}</td><td>Transfer</td></tr>`
@@ -33,14 +36,23 @@ if (checkUser == null) {
   }
   for (let i = 0; i < depositLog.length; i++) {
     two.innerHTML = `<tr class="text-center"> <td>${depositLog[i].depositor}</td> <td class="text-success">$${depositLog[i].amountdeposit}</td>  <td>${depositLog[i].time}</td> <td>${depositLog[i].date}</td></tr>`
-
+    totalCredit += Number(depositLog[i].amountdeposit)
   }
+  console.log(totalCredit);
   for (let i = 0; i < withdrawLog.length; i++) {
     three.innerHTML = `<tr class="text-center"> <td>${withdrawLog[i].withdrawalNames}</td> <td class="text-danger">$${withdrawLog[i].amountWithdraw}</td> <td>${withdrawLog[i].time}</td> <td>${withdrawLog[i].date}</td></tr>`
+    totalDebit += Number(withdrawLog[i].amountWithdraw)
   }
-  // transferLog.filter(() => )
-  // let sum = transferLog.reduce((total, anyVal) => {
-  //   total.type + anyVal.amountSent;
-  //   console.log(total + anyVal.amountSent);
-  // })
+  console.log(totalDebit);
+  
+  const transCredit = transferLog.reduce((tot, record)=> record.transType === 'Credit' ? tot + Number(record.amountSent) : tot , 0)
+  const transDebit = transferLog.reduce((tot, record)=> record.transType === 'Debit' ? tot + Number(record.amountSent) : tot , 0)
+  console.log(transCredit, transDebit)
+  credit.innerHTML = transCredit + totalCredit
+  debit.innerHTML = transDebit + totalDebit
+  console.log(debit.innerHTML, credit.innerHTML);
+//   let totalC = 0
+//   let totalD = 0
+// transferLog.map((val)=> val.transType === "Credit" ? totalC += Number(val.amountSent) :  totalD += Number(val.amountSent))
+// console.log(totalC, totalD)  
 }
